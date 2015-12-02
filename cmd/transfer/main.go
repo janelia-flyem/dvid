98c08bcac0f82b0d6c5d9c7fc0c58d8532cbb630
Bill Katz <billkatz@gmail.com>
2015-12-01 21:21:54 -0500

    add transfer utility

---
+++ cmd/transfer/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math"
+	"os"
+)
+
+var (
+	// Display usage if true.
+	showHelp = flag.Bool("help", false, "")
+
+	// Run in verbose mode if true.
+	runVerbose = flag.Bool("verbose", false, "")
+
+	dryrun = flag.Bool("dryrun", false, "")
+
+	outdir = flag.String("outdir", "", "")
+	url    = flag.String("url", "", "")
+
+	minz = flag.Int("minz", 0, "")
+	maxz = flag.Int("maxz", math.MaxInt32, "")
+
+	compression = flag.String("compress", "gzip", "")
+
+	roi = flag.String("roi", "", "")
+)
+
+const helpMessage = `
+dvid-transfer moves label data from one DVID server to another using HTTP API calls.
+
+Usage: dvid-transfer [options] host uuid name
+
+  where host = URL for the DVID server, e.g., http://host:7000
+        uuid = the UUID with sufficient characters to distinguish version
+        name = name of the label data instance 
+
+  If the destination UUID doesn't already exist, there is an error.
+  If the destination data name doesn't exist, it is created.
+
+	-outdir         =string   Output directory for file output
+	-url            =string   POST URL for DVID, e.g., "http://dvidserver.com/api/653/dataname"
+
+	-compress       =string   Compression for output files.  default "gzip" but allows "none".
+
+	-roi            =string   Instance name of ROI to delimit labels saved
+
+	-minz           =number   Starting Z slice to process.
+	-maxz           =number   Ending Z slice to process.
+
+	-dryrun         (flag)    Don't write files or send POST requests to DVID
+	-verbose    (flag)    Run in verbose mode.
+	-h, -help   (flag)    Show help message
+`
+
+func main() {
+	flag.BoolVar(showHelp, "h", false, "Show help message")
+	flag.Usage = func() {
+		fmt.Printf(helpMessage)
+	}
+	flag.Parse()
+
+	if *showHelp || flag.NArg() != 3 {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	if *url == "" && *outdir == "" {
+		fmt.Printf("Must either use -url and/or -outdir for output!\n")
+		os.Exit(1)
+	}
+
+	host := flag.Args()[0]
+	uuid := flag.Args()[1]
+	name := flag.Args()[2]
+
+	transferData(host, uuid, name)
+}
---
+++ cmd/transfer/transfer.go
@@ -0,0 +1,241 @@
+package main
+
+import (
+	"compress/gzip"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+type BaseMetadata struct {
+	TypeName    string
+	TypeURL     string
+	TypeVersion string
+	Name        string
+	RepoUUID    string
+	Compression string
+	Checksum    string
+	Persistence string
+	Versioned   bool
+}
+
+type Metadata struct {
+	Base     BaseMetadata
+	Extended interface{}
+}
+
+func getLabelMetadata(dstURL string) *LabelMetadata {
+	infoUrl := dstURL + "/info"
+	resp, err := http.Get(infoUrl)
+	if err != nil {
+		fmt.Printf("Error on getting metadata: %s\n", err.Error())
+		os.Exit(1)
+	}
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Bad status on getting metadata: %d\n", resp.StatusCode)
+		os.Exit(1)
+	}
+	metadata, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Could not read metadata: %s\n", err.Error())
+		os.Exit(1)
+	}
+	m := new(LabelMetadata)
+	if err := json.Unmarshal(metadata, m); err != nil {
+		fmt.Printf("Error parsing metadata: %s\n", err.Error())
+		os.Exit(1)
+	}
+	return m
+}
+
+type LabelMetadata struct {
+	Base     BaseMetadata
+	Extended struct {
+		BlockSize [3]int
+		MinIndex  [3]int
+		MaxIndex  [3]int
+	}
+}
+
+func sendLabels(src *LabelMetadata, name, srcURL string) {
+	minIndex := src.Extended.MinIndex
+	maxIndex := src.Extended.MaxIndex
+	blockSize := src.Extended.BlockSize
+	if blockSize[0] != blockSize[1] {
+		fmt.Printf("Can't handle non-cubic block sizes as in %q data instance: %v\n", name, blockSize)
+		os.Exit(1)
+	}
+	fmt.Printf("MinIndex: %v\n", minIndex)
+	fmt.Printf("MaxIndex: %v\n", maxIndex)
+
+	// Compute the output file name
+	var ext string
+	switch *compression {
+	case "none":
+		ext = "dat"
+	case "lz4":
+		ext = "lz4"
+	case "gzip":
+		ext = "gz"
+	default:
+		fmt.Printf("unknown compression type %q", *compression)
+		os.Exit(1)
+	}
+
+	// Iterate through each XY plane of one block deep data.
+	// Send it to destination, synchronously.
+	nx := maxIndex[0] - minIndex[0] + 1
+	ny := maxIndex[1] - minIndex[1] + 1
+
+	// # voxels in x, and xy
+	vx := nx * blockSize[0]
+	vy := ny * blockSize[1]
+	vz := blockSize[2]
+
+	strips := 1
+	// if vx*vy*vz*8 > 2000000000 {
+	// 	strips = (vx*vy*vz*8)/2000000000 + 1
+	// }
+	byPerStrip := ny / strips
+	fmt.Printf("Total uncompressed bytes per file: %d MB\n", vx*vy*vz*8/1000000)
+
+	ox := minIndex[0] * blockSize[0]
+	for z := minIndex[2]; z <= maxIndex[2]; z++ {
+		oz := z * blockSize[2]
+		if oz+blockSize[2] < *minz || oz > *maxz {
+			continue
+		}
+		by0 := minIndex[1]
+		for n := 0; n < strips; n++ {
+			if by0 > maxIndex[1] {
+				break
+			}
+			by1 := by0 + byPerStrip - 1
+			if by1 > maxIndex[1] {
+				by1 = maxIndex[1]
+			}
+
+			oy := by0 * blockSize[1]
+			vy := (by1 - by0 + 1) * blockSize[1]
+			getURL := fmt.Sprintf("%s/raw/0_1_2/%d_%d_%d/%d_%d_%d", srcURL, vx, vy, vz, ox, oy, oz)
+			if *roi != "" {
+				getURL += fmt.Sprintf("?roi=%s", *roi)
+			}
+			var resp http.Response
+			if *dryrun {
+				fmt.Printf("-- would GET %s\n", getURL)
+			} else {
+				resp, err := http.Get(getURL)
+				if err != nil {
+					fmt.Printf("Receive error: %s\n", err.Error())
+					os.Exit(1)
+				}
+				if resp.StatusCode != http.StatusOK {
+					fmt.Printf("Bad status on receiving data: %d\n", resp.StatusCode)
+					os.Exit(1)
+				}
+			}
+			if *url != "" {
+				postURL := fmt.Sprintf("%s/raw/0_1_2/%d_%d_%d/%d_%d_%d", *url, vx, vy, vz, ox, oy, oz)
+				if *dryrun {
+					fmt.Printf("-- would POST %s\n", postURL)
+				} else {
+					resp2, err := http.Post(postURL, "application/octet-stream", resp.Body)
+					if err != nil {
+						fmt.Printf("Transmit error: %s\n", err.Error())
+						os.Exit(1)
+					}
+					if resp2.StatusCode != http.StatusOK {
+						fmt.Printf("Bad status on sending data: %d\n", resp2.StatusCode)
+						os.Exit(1)
+					}
+				}
+			}
+			if *outdir != "" {
+				base := fmt.Sprintf("%s-%dx%dx%d+%d+%d+%d.%s", name, vx, vy, vz, ox, oy, oz, ext)
+				filename := filepath.Join(*outdir, base)
+				if err := writeFile(resp.Body, filename); err != nil {
+					fmt.Printf("Error writing file: %s\n", filename)
+					os.Exit(1)
+				}
+			}
+
+			by0 += byPerStrip
+		}
+	}
+}
+
+func writeFile(data io.ReadCloser, filename string) error {
+	fmt.Printf("Writing data to %s\n", filename)
+	if *dryrun {
+		return nil
+	}
+	defer data.Close()
+
+	// Setup file for write
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	var out io.Writer
+	switch *compression {
+	case "none":
+		out = f
+	case "gzip":
+		out = gzip.NewWriter(f)
+	default:
+		return fmt.Errorf("Can't compress to type %q, only %q\n", *compression, "gzip")
+	}
+
+	io.Copy(out, data)
+	return nil
+}
+
+func transferData(host, uuid, name string) {
+	// If we are transferring to another DVID host, verify the destination.
+	var dstMetadata *LabelMetadata
+	if *url != "" {
+		dstMetadata = getLabelMetadata(*url)
+		dsttype := dstMetadata.Base.TypeName
+
+		switch dsttype {
+		case "labelblk":
+			fmt.Printf("-- verified that POST URL (%v) is labelblk.\n", *url)
+		default:
+			fmt.Printf("POST URL (%v) must be labelblk but is %s data type\n", *url, dsttype)
+			os.Exit(1)
+		}
+	}
+
+	// Get the source metadata
+	srcURL := fmt.Sprintf("%s/api/node/%s/%s", host, uuid, name)
+	srcMetadata := getLabelMetadata(srcURL)
+	if srcMetadata.Base.TypeName != "labelblk" {
+		fmt.Printf("Source data instance %q is not labelblk type.  Aborting.\n", name)
+		os.Exit(1)
+	}
+
+	// If we are writing files to output directory, make sure it's created.
+	// Make sure output directory exists if it's specified.
+	if *outdir != "" {
+		if fileinfo, err := os.Stat(*outdir); os.IsNotExist(err) {
+			fmt.Printf("Creating output directory: %s\n", *outdir)
+			err := os.MkdirAll(*outdir, 0744)
+			if err != nil {
+				fmt.Printf("Can't make output directory: %s\n", err.Error())
+				os.Exit(1)
+			}
+		} else if !fileinfo.IsDir() {
+			fmt.Printf("Supplied output path (%s) is not a directory.", *outdir)
+			os.Exit(1)
+		}
+	}
+
+	sendLabels(srcMetadata, name, srcURL)
+}
