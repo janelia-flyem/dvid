81bba8d627b99748424f8bf9bd03c80c57ab63ba
Bill Katz <billkatz@gmail.com>
2016-03-29 11:27:48 -0400

    remove deprecated datatypes

--- cmd/dvid/main.go
+++ cmd/dvid/main.go
@@ -34,9 +34,6 @@ import (
 	_ "github.com/janelia-flyem/dvid/datatype/labelvol"
 	_ "github.com/janelia-flyem/dvid/datatype/multichan16"
 	_ "github.com/janelia-flyem/dvid/datatype/roi"
-	_ "github.com/janelia-flyem/dvid/datatype/synapse"
-	//_ "github.com/janelia-flyem/dvid/datatype/labelsurf"
-	//_ "github.com/janelia-flyem/dvid/datatype/labelsz"
 )
 
 var (
