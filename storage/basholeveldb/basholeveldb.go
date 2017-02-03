f9deeaf70553b8b370f7cbde6103d88b6f919ce8
Bill Katz <billkatz@gmail.com>
2017-02-03 13:51:57 -0500

    remove unnecessary debug

--- storage/basholeveldb/basholeveldb.go
+++ storage/basholeveldb/basholeveldb.go
@@ -1054,9 +1054,7 @@ func (db *LevelDB) DeleteRange(ctx storage.Context, kStart, kEnd storage.TKey) e
 	// Consume the key-value pairs.
 	numKV := 0
 	for {
-		dvid.Infof("Waiting for result...\n")
 		result := <-ch
-		dvid.Infof("Got result: %v\n", result)
 		if result.KeyValue == nil {
 			break
 		}
