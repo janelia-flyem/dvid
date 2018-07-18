aaec3e85b3ea8b7e9ae34ca10a41350a58f7ca74
Stuart Berg <bergs@janelia.hhmi.org>
2018-07-18 15:54:24 -0400

    _install_compiled_dependencies.py: Python-2 compatible; install all dependencies in one step.

--- scripts/_install_compiled_dependencies.py
+++ scripts/_install_compiled_dependencies.py
@@ -5,7 +5,7 @@ print("Determining compiled dependencies...")
 print("(This may take several seconds.)")
 
 recipe_yaml = '/tmp/dvid-rendered-recipe-meta.yaml'
-subprocess.check_call(f'conda render --file {recipe_yaml} conda-recipe', shell=True)
+subprocess.check_call('conda render --file {} conda-recipe'.format(recipe_yaml), shell=True)
 
 with open(recipe_yaml, 'r') as f:
     recipe_meta = yaml.load(f)
@@ -13,9 +13,10 @@ with open(recipe_yaml, 'r') as f:
 build_requirements = recipe_meta['requirements']['build']
 
 print("Installing compiled dependencies...")
-for requirement_spec in map(lambda r: '='.join(r.split()), build_requirements):
-    cmd = f'conda install -y {requirement_spec}'
-    print(cmd)
-    subprocess.check_call(cmd, shell=True)
+requirement_specs = map(lambda r: '='.join(r.split()), build_requirements)
+
+cmd = 'conda install -y {}'.format(' '.join(requirement_specs))
+print(cmd)
+subprocess.check_call(cmd, shell=True)
 
 print("Done installing compiled dependencies.")
